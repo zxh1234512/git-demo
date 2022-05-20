ae8455e24f8cb9e0bc5eb0d346f6294a8b9f1ffe
zxh1234512 <64399213+zxh1234512@users.noreply.github.com>
2022-05-20 16:03:41 +0800

    Update test1.java

--- src/test1.java
+++ src/test1.java
@@ -2,5 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("push initial");
         System.out.println("push test2");
+        System.out.println("pull test3");
+        
     }
 }
