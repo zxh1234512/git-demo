6741f322baca7dee944c507d210f24d3b5c88022
zxh <2933993542@qq.com>
2022-05-20 15:57:46 +0800

    master-3

--- src/test1.java
+++ src/test1.java
@@ -1,6 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("push initial");
-        System.out.println("push ssh test");
+        System.out.println("push test2");
     }
 }
